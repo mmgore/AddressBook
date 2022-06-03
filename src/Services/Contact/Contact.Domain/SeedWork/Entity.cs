8ac1d123d18c32e6d98af080fdee9ff4dd410ca6
Murat GÖRE <m.muratgore@gmail.com>
2022-06-03 23:53:51 +0300

    added entitybase model

---
+++ src/Services/Contact/Contact.Domain/SeedWork/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.Domain.SeedWork
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
