f90a1596e9e7b5bfe7dbb9458ba492d59e2cc440
Murat GÖRE <m.muratgore@gmail.com>
2022-06-04 22:21:18 +0300

    added entity type configrations

---
+++ src/Services/Contact/Contact.Infrastructure/EntityConfigrations/ContactInformationEntityTypeConfigration.cs
@@ -0,0 +1,40 @@
+﻿using Contact.Domain.AggregatesModel.ContactAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.Infrastructure.EntityConfigrations
+{
+    public class ContactInformationEntityTypeConfigration : IEntityTypeConfiguration<ContactInformation>
+    {
+        public void Configure(EntityTypeBuilder<ContactInformation> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.InformationType.PhoneNumber)
+                .HasMaxLength(15)
+                .IsRequired();
+
+            builder.Property(b => b.InformationType.EmailAddress)
+                .HasMaxLength(40)
+                .IsRequired();
+
+            builder.Property(b => b.InformationType.Location)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(b => b.Content)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.HasOne<ContactItem>()
+                .WithMany()
+                .IsRequired()
+                .HasForeignKey("ContactId");
+        }
+    }
+}
---
+++ src/Services/Contact/Contact.Infrastructure/EntityConfigrations/ContactItemEntityTypeConfigration.cs
@@ -0,0 +1,31 @@
+﻿using Contact.Domain.AggregatesModel.ContactAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.Infrastructure.EntityConfigrations
+{
+    public class ContactItemEntityTypeConfigration : IEntityTypeConfiguration<ContactItem>
+    {
+        public void Configure(EntityTypeBuilder<ContactItem> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.FirstName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(b => b.LastName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(b => b.Firm)
+                .HasMaxLength(60)
+                .IsRequired();
+        }
+    }
+}
