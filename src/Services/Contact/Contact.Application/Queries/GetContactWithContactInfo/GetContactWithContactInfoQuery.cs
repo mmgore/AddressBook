a954c5a730696898a6bc5ddf0b6caedb35bbfc27
Murat GÖRE <m.muratgore@gmail.com>
2022-06-08 01:38:06 +0300

    added  new query

---
+++ src/Services/Contact/Contact.Application/Queries/GetContactWithContactInfo/GetContactWithContactInfoQuery.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.Application.Queries.GetContactWithContactInfo
+{
+    public class GetContactWithContactInfoQuery : IRequest<GetContactWithContactInfoViewModel>
+    {
+        public Guid Id { get; private set; }
+        public GetContactWithContactInfoQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+    
+    public class GetContactWithContactInfoDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Firm { get; set; }
+        public List<ContactInfoDto> ContactInfos { get; set; }
+    }
+
+    public class ContactInfoDto
+    {
+        public Guid ContactId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Location { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class GetContactWithContactInfoViewModel
+    {
+        public IEnumerable<GetContactWithContactInfoDto> ContactInfos { get; set; }
+    }
+}
---
+++ src/Services/Contact/Contact.Application/Queries/GetContactWithContactInfo/GetContactWithContactInfoQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Contact.Domain.AggregatesModel.ContactAggregate;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.Application.Queries.GetContactWithContactInfo
+{
+    public class GetContactWithContactInfoQueryHandler : IRequestHandler<GetContactWithContactInfoQuery, GetContactWithContactInfoViewModel>
+    {
+        private readonly IContactItemRepository _contactItemRepository;
+        private readonly IMapper _mapper;
+        public GetContactWithContactInfoQueryHandler(IContactItemRepository contactItemRepository,
+            IMapper mapper)
+        {
+            _contactItemRepository = contactItemRepository ?? throw new ArgumentNullException(nameof(contactItemRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+        public async Task<GetContactWithContactInfoViewModel> Handle(GetContactWithContactInfoQuery request, CancellationToken cancellationToken)
+        {
+            var contactWithInfos = await _contactItemRepository.GetContactItemsWithInfos(request.Id);
+
+            return null;
+
+        }
+    }
+}
