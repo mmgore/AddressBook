add5c4945e6b52b90c31ff17c00a9e7d8ddfe6c4
Murat GÖRE <m.muratgore@gmail.com>
2022-06-09 21:42:36 +0300

    added application tests

--- src/Services/Contact/Test/Contact.UnitTests/ContactItemTest.cs
+++ src/Services/Contact/Test/Contact.UnitTests/ContactItemTest.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using Contact.Application.Automapper;
 using Contact.Application.Commands.CreateContact;
 using Contact.Application.Commands.UpdateContact;
+using Contact.Application.Exceptions;
+using Contact.Application.Queries.GetContactById;
 using Contact.Application.Validations;
 using Contact.Domain.AggregatesModel.ContactAggregate;
 using Contact.Domain.Exceptions;
@@ -22,6 +25,21 @@ namespace Contact.UnitTests
 {
     public class ContactItemTest
     {
+        private readonly Mock<IContactItemRepository> _contactItemRepositoryMock;
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+        private readonly IMapper _mapper;
+
+        public ContactItemTest()
+        {
+            _contactItemRepositoryMock = new Mock<IContactItemRepository>();
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<AutomapperProfile>();
+            });
+            _mapper = mapperConfig.CreateMapper();
+        }
         [Fact]
         public void Create_contact_item_success()
         {
@@ -38,6 +56,64 @@ namespace Contact.UnitTests
                 .WithMessage("Firm cannot be null");
         }
 
+        [Fact]
+        public async void Should_update_contact_successful()
+        {
+            var fakeContact = ContactItem.Create("fakeName", "fakeLast", "fakeFirm");
+            var fakeFirm = "newFirm";
+
+            var logger = new Mock<ILogger<UpdateContactCommandHandler>>();
+
+            _contactItemRepositoryMock.Setup(r => r.GetContactItemById(fakeContact.Id))
+                .ReturnsAsync(fakeContact);
+
+            _contactItemRepositoryMock.Setup(r => r.UpdateAsync(fakeContact));
+
+            var cmd = new UpdateContactCommand() { Id = fakeContact.Id, FirstName= fakeContact.FirstName, LastName = fakeContact.LastName, Firm = fakeFirm };
+
+            var handler = new UpdateContactCommandHandler(_contactItemRepositoryMock.Object, _unitOfWorkMock.Object, logger.Object, _mapper);
+
+            var sut = await handler.Handle(cmd, CancellationToken.None);
+
+            sut.Should().Be(Unit.Value);
+            fakeContact.Firm.Should().Be(fakeFirm);
+        }
+
+        [Fact]
+        public async void Should_get_contat_by_id_successful()
+        {
+            var fakeContact = ContactItem.Create("fakeName", "fakeLast", "fakeFirm");
+
+            _contactItemRepositoryMock.Setup(r => r.GetContactItemById(It.IsAny<Guid>()))
+                .ReturnsAsync(fakeContact);
+
+            var cmd = new GetContactByIdQuery() { ContactItemId = fakeContact.Id };
+
+            var handler = new GetContactByIdQueryHandler(_contactItemRepositoryMock.Object, _mapper);
+
+            var sut = await handler.Handle(cmd, CancellationToken.None);
+
+            sut.FirstName.Should().Be(fakeContact.FirstName);
+            sut.LastName.Should().Be(fakeContact.LastName);
+            sut.Firm.Should().Be(fakeContact.Firm);
+        }
+
+        [Fact]
+        public async void Should_get_contact_by_id_throw_exception()
+        {
+            var fakeContact = ContactItem.Create("fakeName", "fakeLast", "fakeFirm");
+
+            _contactItemRepositoryMock.Setup(r => r.GetContactItemById(It.IsAny<Guid>()))
+                .ReturnsAsync((ContactItem)null);
+
+            var cmd = new GetContactByIdQuery() { ContactItemId = fakeContact.Id };
+
+            var handler = new GetContactByIdQueryHandler(_contactItemRepositoryMock.Object, _mapper);
+
+            Func<Task> sut = async() => await handler.Handle(cmd, CancellationToken.None);
+
+            sut.Should().ThrowAsync<NotFoundException>();
+        }
 
         private string firsName => "fakeName";
         private string lastName => "fakeLastName";
